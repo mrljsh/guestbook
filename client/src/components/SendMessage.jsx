6f2d02c01dc533fbbdc5869d19e8f3979573126b
Mrljsh <dragan.mrljes@vsar.edu.rs>
2023-05-27 12:04:21 +0200

    feat: adds and styles sendMessage form

--- client/src/components/SendMessage.jsx
+++ client/src/components/SendMessage.jsx
@@ -1,5 +1,46 @@
 const SendMessage = () => {
-  return <div>SendMessage</div>;
+  return (
+    <form className="w-full max-w-lg mx-auto">
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="name"
+        >
+          Name
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="name"
+          type="text"
+          placeholder="Enter your name"
+          required
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="message"
+        >
+          Message
+        </label>
+        <textarea
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="message"
+          rows="4"
+          placeholder="Enter your message"
+          required
+        ></textarea>
+      </div>
+      <div className="flex items-center justify-center">
+        <button
+          className="bg-green-500 hover:bg-green-600 hover:transition-all text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default SendMessage;
