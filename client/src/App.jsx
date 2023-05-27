96376abf656f64344d15f58b58d4245211281245
Mrljsh <dragan.mrljes@vsar.edu.rs>
2023-05-27 11:04:13 +0200

    feat: creates root and send route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import "./App.css";
 import Messages from "./components/Messages";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SendMessage from "./components/SendMessage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Messages />,
+  },
+  {
+    path: "send",
+    element: <SendMessage />,
+  },
+]);
 
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold text-center py-6">Guestbook</h1>
-      <Messages />
+      <RouterProvider router={router} />
     </>
   );
 };
